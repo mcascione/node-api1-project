3cde4e548a0f335b2649e5c3bd4729bb5b98820a
Miranda Cascione <miranda.cascione@gmail.com>
2023-05-02 14:13:15 -0700

    initialized server and wired up get users endpoint

--- api/server.js
+++ api/server.js
@@ -1,3 +1,20 @@
 // BUILD YOUR SERVER HERE
+const express = require('express');
+const User = require('./users/model');
+const server = express();
+server.use(express.json());
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+//get request
+server.get('/api/users', async (req, res) => {
+    try {
+        const users = await User.find();
+        res.status(200).json(users)
+    } catch (err) {
+        res.status(500).json({
+            message: `Error fetching users: ${err.message}`
+        });
+    }
+})
+
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
